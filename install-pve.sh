cff0726deb3296d4c3a3c660c9eb2c9d8c3bac48
jiangcuo <49061187+jiangcuo@users.noreply.github.com>
2023-01-22 22:23:21 +0800

    add local install support
    + installed from isofile
    + installed with local env
    + add judgment before clean rootdisk

--- install-pve.sh
+++ install-pve.sh
@@ -1,48 +1,78 @@
 #!/bin/bash
-sleep 5
-#base_conf
-pve_target="/tmp/target"
-pve_base="/tmp/pve_base-squ"
-#rootdisk="/dev/sda"
+#ReadME
+##########  install_way  ###########
+#install_way="cdrom"  # optional !
+##default is cdrom. apt now has some bug,not recommened
+#########   config_file  ##########
+#config_file="local"  # necessary!
+##how to load config_file from http|cdrom|local
+#-----  http  ------
+#you must set http_conf_url if you want't use http.
+##http_conf_url="http://192.168.3.120:801/msg.conf"
+##msg.conf like
+###mac fq dn  ip_addr cidr  gateway install_way time_zone passwd  pvedisk 
+###EA:5A:F0:5A:B3:D2 pve2 bingsin.com 10.13.14.22 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sda
+###EA:5A:F0:5A:B3:D3 pve3 bingsin.com 10.13.14.23 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sda
+###EA:5A:F0:5A:B3:D4 pve4 bingsin.com 10.13.14.24 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sda
+###EA:5A:F0:5A:B3:D5 pve5 bingsin.com 10.13.14.25 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sdb
+###EA:5A:F0:5A:B3:D6 pve6 bingsin.com 10.13.14.26 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sda
+###EA:5A:F0:5A:B3:D7 pve7 bingsin.com 10.13.14.27 24  10.13.14.252 cdrom Asia/Shanghai P@SSword /dev/sda
+#-----   local  -------
+##if you set config_file="local",You need to configure the following env
+#local_env
+#pve_target="/tmp/target"
+#pve_base="/tmp/pve_base-squ"
+#rootdisk="/dev/sdb"
 #userpw="P@SSw0rd"
-#ipaddr="192.168.3.44"
+#ipaddr="192.168.3.41"
 #netmask="24"
 #gateway="192.168.3.1"
 #eth="enp6s18"
 #fq="pve"
 #dn="bingsin.com"
+#-----------------------------------------------------
+##########  isofile  ###########
+#isofile="/proxmox.iso" # optional !
+## if you don't hava /dev/sr0,you can use proxmox.iso 
+sleep 3
 
-#config the install way,options is "cdrom or apt",default is cdrom.if network is lost, will use cdrom
-#install_way="cdrom"  !! error, apt it's not work .
+errlog(){
+	if [ $? != 0 ];then
+		echo $1
+		exit 0
+	fi
+}
 
-#how to load config_file from http or cdrom (whitch is use local conf-file)
-#you must set http_conf_url if you want't use http.
-config_file="cdrom"
-#http_conf_url="http://192.168.3.120:801/msg.conf"
+
+checkisofile(){
+	if [ ! -z $isofile ];then
+		if [ -d /cdrom ];then
+			umount -l /cdrom
+		else
+			mkdir /cdrom
+		fi
+		mount -t iso9660 -o ro,loop $isofile /cdrom || errlog "can't mount iso file!"
+	fi
+}
 
 copy_roofs(){
-	if [ ! -e /cdrom/pve-base.squashfs ];then
-		echo "pvebasefile is not exist"
-		echo "exit!"
-		exit 0;
-	fi	
 	if [ -d $pve_base  ];then
 		echo "$pve_base is exist"
 		echo "delete!"
 		rm $pve_base -rf
 	fi
 	mkdir  $pve_base 
-	mount  /cdrom/pve-base.squashfs  $pve_base -t squashfs -o loop
+	mount  /cdrom/pve-base.squashfs  $pve_base -t squashfs -o loop  || errlog "can't mount pve-base.squashfs!"
 	echo "copy pve_target"
 	if [ -d $pve_target  ];then
 		echo "$pve_target is exist"
 		echo "delete!"
 		rm $pve_target -rf
 	fi
-	mkdir  $pve_target
-	mount "$rootdisk"3 $pve_target
+	mkdir -p $pve_target
+	mount "$rootdisk"3 $pve_target || errlog "mount rootfs disk error!"
 	cp -ar  $pve_base/* $pve_target
-	umount $pve_base
+	umount -l $pve_base
 }
 
 mount_fstab(){
@@ -138,19 +168,18 @@ install_apt(){
 }
 
 install_dpkg(){
-	umount -l /dev/sr0
-	chroot $pve_target mount /dev/sr0 /media
+	#chroot $pve_target mount /dev/sr0 /media
+	mount -o bind /cdrom $pve_target/media
 	cd $pve_target/media/proxmox/packages/
 	for i in `ls *.deb`;
 	do 
-		DEBIAN_FRONTEND=noninteractive chroot $pve_target dpkg --force-depends --no-triggers --force-unsafe-io --force-confold  --unpack /media/proxmox/packages/$i;
+		LC_ALL=C DEBIAN_FRONTEND=noninteractive chroot $pve_target dpkg --force-depends --no-triggers --force-unsafe-io --force-confold  --unpack /media/proxmox/packages/$i;
 	done
-	DEBIAN_FRONTEND=noninteractive chroot $pve_target dpkg --force-confold --configure --force-unsafe-io -a 
-	chroot $pve_target umount -l /dev/sr0
+	LC_ALL=C DEBIAN_FRONTEND=noninteractive chroot $pve_target dpkg --force-confold --configure --force-unsafe-io -a 
+	umount -l $pve_target/media
 }
 
 copy_proxmox_lib(){
-	proxmox_libdir="/var/lib/proxmox-installer"
     cp $proxmox_libdir/policy-disable-rc.d $pve_target/usr/sbin/policy-rc.d
 	cp $proxmox_libdir/fake-start-stop-daemon $pve_target/sbin/
 }
@@ -187,7 +216,7 @@ grub_install(){
 	chroot $pve_target grub-install --target x86_64-efi --no-floppy --bootloader-id='proxmox' $rootdisk
 	cp $pve_target/boot/efi/EFI/proxmox/grubx64.efi $pve_target/boot/efi/EFI/BOOT/BOOTX64.EFI 
 	echo "create bios boot"
-	chroot $pve_target grub-install --target=i386-pc --recheck --debug $rootdisk
+	chroot $pve_target grub-install --target=i386-pc --recheck --debug $rootdisk >/dev/null 2>&1
 }
 
 config_postfix(){
@@ -208,14 +237,18 @@ config_timezone(){
 
 check_config(){
 	if [ $config_file = "http" ];then
-		wget -P /tmp/ $http_conf_url
-	else
-		if [ ! -z /cdrom/msg.conf ];then
+		wget -P /tmp/ $http_conf_url 
+	elif [ $config_file = "cdrom" ];then 
+		if [ -e /cdrom/msg.conf ];then
 			cp /cdrom/msg.conf /tmp/
 		else
 			echo "no local conf found"
 			exit 0
 		fi
+	elif [ $config_file = "local" ];then
+		echo "config is set to local"
+	else
+	 	echo "config_file $config_file not correct !"
 	fi
 }
 
@@ -252,33 +285,55 @@ load_config(){
 	fi
 }
 
-disk_setup(){
+config_check(){
+	if [ -z $rootdisk ];then
+	errlog "disk not defined"
+	fi
 	if [ ! -b $rootdisk ];then
-		echo "$rootdisk is not exist"
-		echo "exit!"
-		exit 0;
-	fi	
-	#check disk whether exist
-	echo $rootdisk
+		errlog "$rootdisk is not exist"
+	fi
+	if [ ! -z "$(lsblk -f|grep $rootdisk|grep LVM2)" ];then
+		echo "Detected lvm filesystem on,abort!"
+		echo "please remove it and try again."
+		exit 0
+	fi
+	if [ ! -z "$(df -h|grep $rootdisk)" ];then
+		echo "Detected $rootdisk has mounted !"
+		echo "plese umount first"
+		exit 0
+	fi
+	test -f /cdrom/pve-base.squashfs || errlog "can't find pve-base.squashfs! mybe /cdrom not mounted"
+	export proxmox_libdir="/var/lib/proxmox-installer"
+	test -d $proxmox_libdir || errlog "no proxmox-installer found"
+}
 
+disk_setup(){
 	dd if=/dev/zero of=$rootdisk bs=1M count=16
 	echo "create gpt"
-	sgdisk -Z $rootdisk
-
+	sgdisk -GZ $rootdisk >/dev/null 2>&1
+	
 	echo "create bios parttion"
-	sgdisk -a1 -n1:34:2047  -t1:EF02  $rootdisk
+	sgdisk -a1 -n1:34:2047  -t1:EF02  $rootdisk >/dev/null 2>&1
 
 	echo "create efi parttion"
-	sgdisk -a1 -n2:1M:+512M -t2:EF00 $rootdisk
+	sgdisk -a1 -n2:1M:+512M -t2:EF00 $rootdisk >/dev/null 2>&1
 	mkfs.vfat -F 32 "$rootdisk"2
 
 	echo "create root parttion"
-	sgdisk -a1 -n3:513M:-1G  $rootdisk
+	sgdisk -a1 -n3:513M:-1G  $rootdisk >/dev/null 2>&1
 	mkfs.ext4 -F "$rootdisk"3
 }
 
+config_passwd(){
+	echo "root:$userpw" |chroot $pve_target chpasswd
+}
+
+checkisofile
 check_config
-load_config
+if [  $config_file != "local" ];then
+	load_config
+fi
+config_check
 disk_setup
 copy_roofs
 mount_fstab
@@ -290,10 +345,14 @@ diversion_add $pve_target /sbin/start-stop-daemon /sbin/fake-start-stop-daemon
 diversion_add $pve_target /usr/sbin/update-grub /bin/true
 diversion_add $pve_target /usr/sbin/update-initramfs /bin/true
 
-if [ $install_way = "apt" ];then
-	install_apt
-else
+if [ -z $install_way ];then
 	install_dpkg
+elif [ $install_way = "apt" ];then
+	install_apt
+elif [ $install_way = "cdrom" ];then
+	install_dpkg
+else
+	echo "install_way not corrected"
 fi
 
 clean_proxmox_lib
@@ -305,6 +364,6 @@ grub_install
 enable_ssh
 apt_mirrors
 config_timezone
-
+config_passwd
 clean_chroot
-reboot -f
+#reboot -f
